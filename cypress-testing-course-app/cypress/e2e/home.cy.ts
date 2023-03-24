cfaeae8bdabeed8eef93beac8669c5781923284b
MohammadMzien2 <mohammadlu281@gmail.com>
2023-03-24 11:17:58 +0100

    test the features on the homepage are correct.

--- cypress-testing-course-app/cypress/e2e/home.cy.ts
+++ cypress-testing-course-app/cypress/e2e/home.cy.ts
@@ -1,6 +1,9 @@
 describe('Home Page', () => {
+  beforeEach(() => {
+    cy.visit("http://localhost:3000")
+  })
+
   it('the h1 contains the correct tex', () => {
-    cy.visit('http://localhost:3000')
 
     cy.get("[data-test='hero-heading']")
       .should('exist')
@@ -8,9 +11,16 @@ describe('Home Page', () => {
   })
 
   it("the features on the homepage are correct", () => {
-    cy.visit('http://localhost:3000')
     cy.get("dt")
     .eq(0)
-    .contains("4 courses")
+    .contains("4 Courses")
+
+    cy.get("dt")
+    .eq(1)
+    .contains("25+ Lessons")
+
+    cy.get("dt")
+    .eq(2)
+    .contains("Free and Open Source")
   })
 })
