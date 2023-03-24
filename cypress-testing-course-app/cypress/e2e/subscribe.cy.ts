062076b88bf808429c9021a7cc9b0a59a3247eaa
MohammadMzien2 <mohammadlu281@gmail.com>
2023-03-24 13:37:10 +0100

    added faker to fake visiuos test

--- cypress-testing-course-app/cypress/e2e/subscribe.cy.ts
+++ cypress-testing-course-app/cypress/e2e/subscribe.cy.ts
@@ -1,12 +1,24 @@
-describe('Newsletter Subscribe', () => {
-    beforeEach(() => {
-        cy.visit('http://localhost:3000')
-    })
+import { faker } from '@faker-js/faker'
 
-    it('allows users to subscribe to the email list', () => {
-        //cy.get('[data-test="email-input"]')
-        cy.getByDataTest('email-input').type('pella@svanlos.nu')
-        cy.getByDataTest('submit-button').click
-        cy.getByDataTest('success-message').should('exist').contains('pella@svanslos.nu')
-    })
+describe('Newsletter Subscribe', () => {
+	const email = faker.internet.email()
+	const invalidEmail = 'pelle'
+	const subscribedEmail = 'john@example.com'
+
+	beforeEach(() => {
+		cy.visit('http://localhost:3000')
+	})
+
+	it('allows users to subscribe to the email list', () => {
+		// cy.get('[data-test="email-input"]')
+		cy.getByDataTest('email-input').type(email)
+		cy.getByDataTest('submit-button').click()
+		cy.getByDataTest('success-message').should('exist').contains(email)
+	})
+
+	it.skip('displays an error message when the email is invalid')
+
+    it.skip('should not allow users to subscribe twice')
+
+
 })
