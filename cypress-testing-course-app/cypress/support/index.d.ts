ed7ea17ea357103b4724a4eadbda532df95d8acb
MohammadMzien2 <mohammadlu281@gmail.com>
2023-03-24 13:14:47 +0100

    Added custom Cypress command getByDataTest as well as a happy-test

---
+++ cypress-testing-course-app/cypress/support/index.d.ts
@@ -0,0 +1,10 @@
+declare namespace Cypress {
+	interface Chainable {
+		/**
+		 * Custom command to select DOM element by data-test attribute.
+		 * @example cy.getByDataTest('email-input')
+		 */
+		getByDataTest(selector: string): Chainable<JQuery<HTMLElement>>
+
+	}
+}
